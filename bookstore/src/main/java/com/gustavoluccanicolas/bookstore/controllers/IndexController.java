61274398f7ade7736d773fcb5ee0d0f4e7a41e30
Ncjhsn <niceniick@gmail.com>
2020-07-01 21:49:43 -0300

    navegacao entre as paginas

--- bookstore/src/main/java/com/gustavoluccanicolas/bookstore/controllers/IndexController.java
+++ bookstore/src/main/java/com/gustavoluccanicolas/bookstore/controllers/IndexController.java
@@ -1,35 +1,28 @@
 package com.gustavoluccanicolas.bookstore.controllers;
 
 import com.gustavoluccanicolas.bookstore.models.Livro;
-import com.gustavoluccanicolas.bookstore.models.enums.Genero;
 import com.gustavoluccanicolas.bookstore.repositories.LivroRepository;
+import com.gustavoluccanicolas.bookstore.services.PaginaService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class IndexController {
 
     private final LivroRepository livroRepository;
-
-    public IndexController(LivroRepository livroRepository) {
+    private final PaginaService paginaService;
+    public IndexController(LivroRepository livroRepository, PaginaService paginaService) {
         this.livroRepository = livroRepository;
+        this.paginaService = paginaService;
     }
 
     @RequestMapping("/")
     public String index(){
         return "index";
-    }
-
-    @GetMapping("/")
-    public ModelAndView findAllBooks(){
-        List<Livro> livro = livroRepository.findAll();
-        ModelAndView model = new ModelAndView("index");
-        return model.addObject("livros", livro);
     }
 
 }
